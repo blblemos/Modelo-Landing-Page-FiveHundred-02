54d0a54e27d759a2ff32ab394d180b3bb122e166
blblemos <blblemos17@gmail.com>
2020-12-28 17:32:01 -0300

    menuMobile ok

---
+++ JS/openMenuMobile.js
@@ -0,0 +1,20 @@
+function openMenuMobile(){
+    var x = document.getElementById("menuMobile");
+        if (x.style.display === "block") {
+            x.style.display = "none";
+        } else {
+            x.style.display = "block";
+        }
+}
+
+function closeMenuMobile() {
+        
+   
+}
+
+$(function() {
+    $('nav.menu-mobile ul li a').click(function() {
+        document.getElementById("menuMobile").style.display = "none";
+        return false;
+    })
+});
