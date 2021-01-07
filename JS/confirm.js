a0cd7b0ab2f1fbf6ee9dad90bcd934b707ba67bb
blblemos <blblemos17@gmail.com>
2021-01-07 18:59:07 -0300

    upgrade codacy 2

--- JS/confirm.js
+++ JS/confirm.js
@@ -1,16 +1,15 @@
 
 $(function(){
     function openModal() {
-        var html =`
+        document.getElementById("modal").innerHTML = (`
         <div class="modal">
             <div class="div-modal">
                 <a href="index.html"><p class="close-modal">&times;</p></a>
                 <p class="text-modal" >Seus dados já estão conosco, entraremos em contato em breve.</p>
             </div>
-        </div>`;
-        document.getElementById("modal").innerHTML = (html);
+        </div>`);
         event.preventDefault();
-    };
+    }
 
-    document.getElementById("form-modal").onsubmit = function() {openModal()};
+    document.getElementById("form-modal").onsubmit = function() {openModal();};
 });
